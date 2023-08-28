4f309c6318d5e5760b88f39efd33721197efc768
Mohamed Zakaria <medozakaria014@gmail.com>
2023-08-28 19:56:05 +0200

    mod register view

--- Library/libraryStore/user/views.py
+++ Library/libraryStore/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import RegisterForm
 from django.contrib.auth.hashers import make_password
 # Create your views here.
@@ -10,4 +10,5 @@ def register(request):
         form = RegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
+            return HttpResponse("tmmam")
     return render(request, 'user/register.html', {'form': form})
