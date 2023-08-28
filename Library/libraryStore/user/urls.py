890c247c4add4b9ddb58862fac04737f9ed222f0
Mohamed Zakaria <medozakaria014@gmail.com>
2023-08-28 21:56:01 +0200

    add basic  login and logout functionality

--- Library/libraryStore/user/urls.py
+++ Library/libraryStore/user/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('register', views.register)
+    path('register', views.register),
+    path('login', views.Login.as_view()),
+    path('logout', views.Logout.as_view()),
 ]
--- Library/libraryStore/user/views.py
+++ Library/libraryStore/user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import RegisterForm
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -12,3 +15,11 @@ def register(request):
             form.save()
             return HttpResponse("tmmam")
     return render(request, 'user/register.html', {'form': form})
+
+
+class Login(LoginView):
+    template_name = "user/login.html"
+
+
+class Logout(LogoutView):
+    template_name = "user/logout.html"
