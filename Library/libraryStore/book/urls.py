9bb2670a831933c780b37538cb6eb457f535d740
MohamedKhaled81 <mohamed.khaled.200540@gmail.com>
2023-09-07 22:07:48 +0300

    adding logic

--- Library/libraryStore/book/urls.py
+++ Library/libraryStore/book/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views 
 
 urlpatterns = [
-    
+    path('<str:cat>/', views.showAllSubBooks, name='Cat-Page'),
+    path('<str:sub>/', views.showSpecificSubBooks, name='sub&Cat-Page')
 ]
--- Library/libraryStore/book/views.py
+++ Library/libraryStore/book/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Books
 
-# Create your views here.
+def showAllSubBooks(request, cat):
+    books = Books.objects.filter(sub_category__category__title=cat)
+    print(books)
+    print(Books.objects.all())
+    context = {"books" : books}
+    return render(request, 'book/books.html', context)
 
+def showSpecificSubBooks(request, sub):
+    books = Books.objects.select_related(sub_category=sub)
+    context = {"books" : books}
+    return render(request, 'book/books.html', context)
