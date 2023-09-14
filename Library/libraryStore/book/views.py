6679c8701632ebe45b7bc94049caab6657794145
Mohamed Zakaria <medozakaria014@gmail.com>
2023-09-14 20:02:59 +0200

    fix views

--- Library/libraryStore/book/views.py
+++ Library/libraryStore/book/views.py
@@ -10,11 +10,8 @@ def show_all_books(request, cat):
 
 # for showing the related books for each sub category
 def show_related_books(request, cat, sub):
-    print(request.path)
     #books = Books.objects.filter(Q(sub_category__category__slug=cat) &  Q(sub_category__slug=sub))
     # books = Books.objects.select_related('sub_category').filter(sub_category__slug=sub)     
-    books = Books.objects.filter(sub_category__slug=sub)         
-    print(books)                   
-    print('dasd')                 
+    books = Books.objects.filter(sub_category__slug=sub)                        
     context = {"books" : books}
     return render(request, 'book/books.html', context)
