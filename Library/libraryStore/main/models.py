469ab348a63bc35415ff1d239d04b52338e8e645
MohamedKhaled81 <mohamed.khaled.200540@gmail.com>
2023-10-10 01:14:25 +0300

    Renaming the Contact Table

--- Library/libraryStore/main/models.py
+++ Library/libraryStore/main/models.py
@@ -5,3 +5,6 @@ class Contact(models.Model):
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     mssg = models.TextField(verbose_name='Message')
+
+    class Meta:
+        db_table = "Contact"
