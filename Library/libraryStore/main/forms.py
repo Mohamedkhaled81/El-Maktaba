919e645a2e8baba6698ec8f7b97bb80260b1ce9e
MohamedKhaled81 <mohamed.khaled.200540@gmail.com>
2023-10-10 00:49:12 +0300

    finish

--- Library/libraryStore/main/forms.py
+++ Library/libraryStore/main/forms.py
@@ -5,6 +5,3 @@ class ContactUsForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
-        widgets = {
-            'mssg': forms.Textarea(attrs={'rows':7, 'cols': 40}),  # Customize the text area
-        }
