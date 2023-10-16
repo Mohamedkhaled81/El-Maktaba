6094d7dbc72b8a24e031e0666eb98f1c10d3ac5d
Mohamed Zakaria <medozakaria014@gmail.com>
2023-10-17 01:21:07 +0200

    improving ui and fix images

--- Library/libraryStore/book/urls.py
+++ Library/libraryStore/book/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('<str:cat>', views.category_related_books, name='Cat-Page'),
     path('<str:cat>/<str:sub>', views.subcategory_related_books, name='sub&Cat-Page'),
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Library/libraryStore/main/urls.py
+++ Library/libraryStore/main/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('home/', views.home, name='Home-Page'),
-    path('books/', include('book.urls')),
     path('about/', views.about, name='About-Page'),
     path('ContactUs/', views.ContactUs, name='ContactUs-Page')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
