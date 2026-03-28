ea1e6175442e52cd1fbd6429562b22b4150dc64f
Mohamed Khaled <mohamed.khaled.200540@gmail.com>
2026-03-28 15:15:29 +0200

    Refactoring Controllers

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,41 +1,17 @@
 import BookModel from "../models/book.model.js";
 import commaToArray from "../utils/commaToArray.js";
 
-const renderAllBooks = async function (req, res) {
-    const books = await BookModel.find();
-    res.render('allBooks', {books});
-};
-const renderAddBook = function (req, res) {
-    res.render('addBook');
-};
-
-const renderEditBook = async function (req, res) {
-    const id = req.params.id;
-    const book = await BookModel.findById(id);
-    res.render('editBook', {book});
-}
-
-//! Handle This Controller in more cleaner way
+//* CRUD ON BOOK RESOURCE
 const addBook = async function (req, res) {
-    try{
-        let {title, publishedYear, authors, categories, description} = req.body;
-        if(!title || ! publishedYear || !authors || !categories || !description) {
-            //! GLOBAL ERROR Handling is required
-            throw new Error('Bad request');
-        }
-        publishedYear = Number(publishedYear);
-        if(Number.isNaN(publishedYear)) {
-            throw new Error('Bad request');
-        }
-        authors = commaToArray(authors);
-        categories = commaToArray(categories);
+    let {title, publishedYear, authors, categories, description} = req.body;
     
-        const book = new BookModel({title, publishedYear, authors, categories, description});
-        await book.save();
-    }
-    catch(err) {
-        res.send('ERROR');
-    }
+    publishedYear = Number(publishedYear);
+    authors = commaToArray(authors);
+    categories = commaToArray(categories);
+    
+    const book = new BookModel({title, publishedYear, authors, categories, description});
+    await book.save();
+
     res.redirect('/books/allBooks');
 };
 
@@ -47,13 +23,35 @@ const updateBook = async function (req, res) {
     payLoad.categories = commaToArray(payLoad.categories);
 
     await BookModel.findByIdAndUpdate(id, payLoad);
-    res.redirect('/books/allBooks');
-};
-const deleteBook = async function (req, res) {
-    const _id = req.params.id;
-    await BookModel.deleteOne({_id});
+
     res.redirect('/books/allBooks');
 };
 
+const deleteBook = async function (req, res) {
+    const _id = req.params.id;
+
+    await BookModel.deleteOne({_id});
+
+    res.redirect('/books/allBooks');
+};
+
+//* Handling RENDERING FUNCTIONS
+const renderAllBooks = async function (req, res) {
+    const books = await BookModel.find();
+
+    res.render('allBooks', {books});
+};
+
+const renderAddBook = function (req, res) {
+    res.render('addBook');
+};
+
+const renderEditBook = async function (req, res) {
+    const id = req.params.id;
+
+    const book = await BookModel.findById(id);
+
+    res.render('editBook', {book});
+}
 
 export {renderEditBook, renderAddBook, renderAllBooks, addBook, updateBook, deleteBook};
